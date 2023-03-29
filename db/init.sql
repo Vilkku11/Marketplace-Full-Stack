169f725a4c292e100cbeb528a9fc160d63aa6382
William Reima <william.reima@gmail.com>
2023-03-29 19:51:38 +0300

    added docker

---
+++ db/init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `listings` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(60) NOT NULL,
+    `prize` int(11) NOT NULL,
+    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `listings` (`name`, `price`) VALUES (`tuote` , 90);
