e9ca285401ad2594079ccd5b147987f491c80e86
William Reima <william.reima@tuni.fi>
2023-05-05 16:06:50 +0300

    test

--- frontend/src/users/pages/Profile.jsx
+++ frontend/src/users/pages/Profile.jsx
@@ -3,7 +3,7 @@ import Listings from "../../listings/pages/Listings";
 const Profile = () => {
   return (
     <div>
-      <h2>Your listings</h2>
+      <h2>Your listings:</h2>
       <Listings profile={true} />
     </div>
   );
