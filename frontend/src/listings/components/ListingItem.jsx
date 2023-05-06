b92e5af7e3a8a5835bca0aa7b45f5d68df424bcb
William Reima <william.reima@gmail.com>
2023-05-06 23:06:35 +0300

    fixed listing

--- frontend/src/listings/components/ListingItem.jsx
+++ frontend/src/listings/components/ListingItem.jsx
@@ -31,7 +31,7 @@ const ListingItem = (props) => {
         Delete
       </Button>
     );
-  } else {
+  } else if (auth.token) {
     buyButton = (
       <Button onClick={listingDeleteHandler} variant="success">
         Buy
@@ -44,10 +44,9 @@ const ListingItem = (props) => {
       <Card>
         <Card.Body>
           <h2 className="text-center">{props.name}</h2>
-          <p>{props.price}</p>
+          <p>Price: {props.price}</p>
           <p>{props.created}</p>
-          <p>{props.user}</p>
-          <p>{props.userId}</p>
+          <p>User: {props.user}</p>
           <img src={props.image} alt={props.image}></img>
           <div>{deleteButton}</div>
           <div>{buyButton}</div>
