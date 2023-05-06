65085775c888cec54bf44ad309b1651a4817e48f
William Reima <william.reima@gmail.com>
2023-05-06 23:31:25 +0300

    added comment

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { loginUser, signUpUser } = require("../controllers/users");
-
+// routes
 router.post("/signup", signUpUser);
 router.post("/login", loginUser);
 
--- frontend/src/listings/components/Listings.test.jsx
+++ frontend/src/listings/components/Listings.test.jsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import ListingsList from "./ListingsList";
+// test change
 
 describe("The ListingsList", () => {
   it("true to be truthy", () => {
